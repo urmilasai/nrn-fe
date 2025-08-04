30f1c743274596cebcf2a98de29091b18e6f964e
Urmila <thakuri374@gmail.com>
2025-08-04 11:19:27 +0930

    safari fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta property="og:title" content="NRN News - Latest Updates" />
-        <meta property="og:description" content="Get the latest news and updates from NRN News." />
-        <meta property="og:image" content="https://nrn.news/about.png" /> 
-        <meta property="og:type" content="website" />
+        
       </Head>
       <body className="antialiased">
         <Main />
